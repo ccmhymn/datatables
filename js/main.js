6644109207992e1f7d99c7eaf147e67bcb4fcf47
ccmhymn <71312023+ccmhymn@users.noreply.github.com>
2020-09-18 23:18:00 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,16 +18,16 @@ $(document).ready(function() {
             ],
 
         columns: [
-            {data: "장", title: "장"},         
-            {data: "제목", title: "제목"},
-            {data: "분류", title: "분류"}, 
-            {data: "코드", title: "코드"}, 
-            {data: "박자", title: "박자"}, 
-            {data: "Full가사"},          
-            {data: "Trim가사"},
-            {data: "악보-ID"},
-            {data: "미디-ID"},
-            {data: "가사-ID"}
+            {data: "no", title: "장"},         
+            {data: "title", title: "제목"},
+            {data: "category", title: "분류"}, 
+            {data: "chord", title: "코드"}, 
+            {data: "beat", title: "박자"}, 
+            {data: "lyrics"},          
+            {data: "full_lyrics"},
+            {data: "img"},
+            {data: "midi"},
+            {data: "txt"}
         ],
 
             columnDefs: [{
@@ -74,14 +74,16 @@ $(document).ready(function() {
         $('#myTable tbody').on('click', 'tr', function() {
             var data = table.row(this).data();
             var sJson = JSON.stringify(data);
+          
             $('#rowData').html("All Data : " + sJson);
-            alert('You clicked on ' + data['Full가사'] + '\'s row');
-            var simpleTitle = data['제목'];
-            var title = data['장'] + ". " + data['제목'] + " | " + data['분류'] + " | " + data['코드'] + " | " + data['박자'];
-            var fullText = data['Full가사']; // 가사전체
-            var imgUrl = "https://drive.google.com/uc?id=" + data['악보-ID']; // 이미지 다이렉트 링크
-            var midiUrl = "https://drive.google.com/uc?export=download&id=" + data['미디-ID']; // 미디 다운로드 링크
-            var textUrl = "https://drive.google.com/file/d/" + data['가사-ID'] + "/preview"; // 가사 미리보기 링크
+            alert(data['no'] + '장. ' + data['title'] + '를 클릭하셨습니다.');
+          
+            var simpleTitle = data['title'];
+            var title = data['no'] + ". " + data['title'] + " | " + data['category'] + " | " + data['chord'] + " | " + data['beat'];
+            var fullText = data['full_lyrics']; //가사전체
+            var imgUrl = "/asset/hymn/img/" + data['img']; // 이미지 다이렉트 링크
+            var midiUrl = "/asset/hymn/mid/" + data['midi']; // 미디 다운로드 링크
+            var textUrl = "/asset/hymn/lyrics/" + data['txt'] + "/preview"; // 가사 미리보기 링크
             var youtubeUrl = "https://www.youtube.com/results?search_query=" + simpleTitle;
 
             $('#title').html(title);
@@ -90,7 +92,8 @@ $(document).ready(function() {
             $('#youtubeLink').attr('href', youtubeUrl);
             $('#fullText').html('<div class="ui top right attached label">가사</div>' + fullText);
             $('#iframeText').attr('src', textUrl);
-            //alert(simpleTitle + fullText + textUrl + youtubeUrl);
+          
+            $('#rowData').append(imgUrl + "<img src='" + imgUrl + "'/>");
 
 
 
