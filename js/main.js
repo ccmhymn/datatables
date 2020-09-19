c739f5a2ebc3f49da2f27b13e01f4816c7386b12
ccmhymn <71312023+ccmhymn@users.noreply.github.com>
2020-09-19 22:27:39 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         var table = $('#myTable').DataTable({
             data: data,
             select: true,
-            responsive: false,
+            responsive: true,
             orderMulti: true,
             processing: true,
             fixedHeader: true,
