db8afb6af59343508c66e7e8bd36fb8f3f813c65
ccmhymn <71312023+ccmhymn@users.noreply.github.com>
2022-06-03 16:14:39 +0900

    Update midi.js

--- js/midi.js
+++ js/midi.js
@@ -53,9 +53,9 @@ function tick(song, stepDuration) {
 			});
 			
 		
-            currentSongTime = currentSongTime - song.duration;
-            sendNotes(song, songStart, 0, currentSongTime, audioContext, input, player);
-            songStart = songStart + song.duration;
+            //currentSongTime = currentSongTime - song.duration;
+            //sendNotes(song, songStart, 0, currentSongTime, audioContext, input, player);
+            //songStart = songStart + song.duration;
         }
     }
     if (nextPositionTime < audioContext.currentTime) {
