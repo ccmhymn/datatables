adb8ec26b86d4e4db571621ba2f88aebb768f8e6
ccmhymn <71312023+ccmhymn@users.noreply.github.com>
2022-06-03 14:30:41 +0900

    Update midi.js

--- js/midi.js
+++ js/midi.js
@@ -305,6 +305,9 @@ function tick(song, stepDuration) {
         o.value = 100 * currentSongTime / song.duration;
         document.getElementById('percent').innerHTML = '' + Math.round(100 * currentSongTime / song.duration) + '%';
         nextPositionTime = audioContext.currentTime + 3;
+	    console.log(song.duration);
+	    console.log(audioContext.currentTime);	    
+	    
     } else if  (song.duration == audioContext.currentTime) {
 	    console.log("end");
 	    alert("end");
